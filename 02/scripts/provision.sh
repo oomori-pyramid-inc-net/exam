67da1658e7741ad157c4c02ef45c1f9d158a7c2e
oomori-pyramid-inc-net <oomori@pyramid-inc.net>
2016-04-21 16:48:09 +0900

    exam-02実装完了
    ホストOS側で images -> assets のシンボリックリンクを作成して対応しました。

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -4,6 +4,10 @@ set -eu
 yum -y update
 yum -y install ntp httpd
 
-sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/^Listen 80$/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
+
+sed -i -e "s/#EnableMMAP off/EnableMMAP off/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/#EnableSendfile off/EnableSendfile off/g" /etc/httpd/conf/httpd.conf
+sed -i -e '/Options/s/ Indexes / -Indexes /g' /etc/httpd/conf/httpd.conf
 
