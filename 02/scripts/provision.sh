5f9e386e8846802930ba06145b77e048951bce51
oomori-pyramid-inc-net <oomori@pyramid-inc.net>
2016-04-22 16:57:11 +0900

    exam-02実装完了
    apacheのredirect設定で/imagesへのリクエストをlocalhost:5001/assetsにリダイレクトさせることで対応しました

--- 02/scripts/provision.sh
+++ 02/scripts/provision.sh
@@ -4,6 +4,16 @@ set -eu
 yum -y update
 yum -y install ntp httpd
 
-sed -i -e "s/Listen 80/Listen 8080/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/^Listen 80$/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
 
+sed -i -e "s/#EnableMMAP off/EnableMMAP off/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/#EnableSendfile off/EnableSendfile off/g" /etc/httpd/conf/httpd.conf
+sed -i -e '/Options/s/ Indexes / -Indexes /g' /etc/httpd/conf/httpd.conf
+
+# リダイレクト設定がなければ追加 
+REDIRECT="Redirect permanent /images http://localhost:5001/assets"
+CONF_PATH="/etc/httpd/conf/httpd.conf"
+if ! grep -q "$REDIRECT" "$CONF_PATH"; then
+  sed -i -e "$ a $REDIRECT" "$CONF_PATH"
+fi
