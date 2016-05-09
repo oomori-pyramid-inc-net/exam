525308d95df429955407ba167358d2f0bb4a88f9
oomori-pyramid-inc-net <oomori@pyramid-inc.net>
2016-05-09 19:25:23 +0900

    課題04: 実装終了

--- 04/scripts/provision.sh
+++ 04/scripts/provision.sh
@@ -2,8 +2,44 @@
 set -eu
 
 yum -y update
-yum -y install httpd
+yum -y install httpd ntp
+curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh | sh
 
 sed -i -e "s/Listen 80$/Listen 8080/g" /etc/httpd/conf/httpd.conf
 sed -i -e "s/#ServerName www.example.com:80/ServerName localhost:8080/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/#EnableMMAP off/EnableMMAP off/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/#EnableSendfile off/EnableSendfile off/g" /etc/httpd/conf/httpd.conf
+sed -i -e '/Options/s/ Indexes / -Indexes /g' /etc/httpd/conf/httpd.conf
 
+
+# td-agent用の設定がなければ追加する
+COMMENT="# For td-agent"
+CONF_PATH="/etc/sysctl.conf"
+if ! grep -q "$COMMENT" "$CONF_PATH"; then
+cat << EOS >> "$CONF_PATH"
+
+$COMMENT
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.ip_local_port_range = 10240    65535
+EOS
+	sysctl -w
+fi
+
+# config.d/*.confを読み込むようにする
+CONF_PATH="/etc/td-agent/td-agent.conf"
+CONFIGD_PATH="/etc/td-agent/config.d"
+COMMENT="# Load config files from the config directory \"$CONFIGD_PATH\"."
+mkdir -p "$CONFIGD_PATH"
+if ! grep -q "$COMMENT" "$CONF_PATH"; then
+cat << EOS >> "$CONF_PATH"
+
+$COMMENT
+@include $CONFIGD_PATH/*.conf
+EOS
+fi
+cp /vagrant/conf/td-agent/apache.conf /etc/td-agent/config.d/apache.conf
+
+# ログの読み書き用に権限変更
+chmod +rx /var/log/httpd
+chmod 777 /var/www/html/logs
--- 04/scripts/run.sh
+++ 04/scripts/run.sh
@@ -3,7 +3,7 @@ set -eu
 
 function is_running() {
 	SERVICE_NAME=$1
-	IS_SERVICE_RUNNING=$(service $SERVICE_NAME status | grep "running")
+	IS_SERVICE_RUNNING=$(service $SERVICE_NAME status | grep "is running")
 	if [ "$IS_SERVICE_RUNNING" == "" ]; then
 		echo 'false'
 	else
@@ -11,9 +11,23 @@ function is_running() {
 	fi
 }
 
+IS_NTPD_RUNNING=$(is_running ntpd)
+if [ "$IS_NTPD_RUNNING" == 'true' ]; then
+  service ntpd restart
+else
+  service ntpd start
+fi
+
 IS_HTTPD_RUNNING=$(is_running httpd)
 if [ "$IS_HTTPD_RUNNING" == 'true' ]; then
   service httpd restart
 else
   service httpd start
+fi
+
+IS_TDAGENT_RUNNING=$(is_running td-agent)
+if [ "$IS_TDAGENT_RUNNING" == 'true' ]; then
+  service td-agent restart
+else
+  service td-agent start
 fi
