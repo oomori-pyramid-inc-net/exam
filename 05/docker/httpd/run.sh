c861ffa32e1ca70dcf64e33485ecc793832e3b63
oomori-pyramid-inc-net <oomori@pyramid-inc.net>
2016-06-03 18:31:40 +0900

    課題05b : typo修正 environment -> environment

--- 05/docker/httpd/run.sh
+++ 05/docker/httpd/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-: ${FLUENTD_HEALTHCHECK_ENDPOINT:?"undefined environmet value : FLUENTD_HEALTHCHECK_ENDPOINT"}
+: ${FLUENTD_HEALTHCHECK_ENDPOINT:?"undefined environment value : FLUENTD_HEALTHCHECK_ENDPOINT"}
 
 until wget -q -O - http://${FLUENTD_HEALTHCHECK_ENDPOINT} >/dev/null 2>&1
 do
